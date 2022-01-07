81565336422a8a3c87bd9f713d0163089e09a426
jefflunt <jefflunt@gmail.com>
2022-01-06 23:02:14 -0600

    Add 'grey' and 'gray' aliases for light_black

--- lib/colorize/class_methods.rb
+++ lib/colorize/class_methods.rb
@@ -61,14 +61,15 @@ module Colorize
     def color_codes
       {
         :black   => 0, :light_black    => 60,
-        :red     => 1, :light_red      => 61,
-        :green   => 2, :light_green    => 62,
-        :yellow  => 3, :light_yellow   => 63,
-        :blue    => 4, :light_blue     => 64,
-        :magenta => 5, :light_magenta  => 65,
-        :cyan    => 6, :light_cyan     => 66,
-        :white   => 7, :light_white    => 67,
-        :default => 9
+        :red     => 1, :gray           => 60,
+        :green   => 2, :grey           => 60,
+        :yellow  => 3, :light_red      => 61,
+        :blue    => 4, :light_green    => 62,
+        :magenta => 5, :light_yellow   => 63,
+        :cyan    => 6, :light_blue     => 64,
+        :white   => 7, :light_magenta  => 65,
+        :default => 9, :light_cyan     => 66,
+                       :light_white    => 67,
       }
     end
 
